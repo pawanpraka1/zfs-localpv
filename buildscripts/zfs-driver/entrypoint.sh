5b6358443b3c04aa82f59b9f8ce142f99b4c0eac
Pawan <pawan@mayadata.io>
2020-09-03 17:19:27 +0530

    yaml
    Signed-off-by: Pawan <pawan@mayadata.io>

--- buildscripts/zfs-driver/entrypoint.sh
+++ buildscripts/zfs-driver/entrypoint.sh
@@ -1,5 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ex
 
-/usr/local/bin/zfs-driver
+echo "[entrypoint.sh] launching zfs-driver."
+
+export PATH="/zfspv/bin:$PATH"
+export LD_LIBRARY_PATH="/zfspv/lib"
+
+/usr/local/bin/zfs-driver "$@" &
+
+child=$!
+
+#sigterm caught SIGTERM signal and forward it to child process
+_sigterm() {
+  echo "[entrypoint.sh] caught SIGTERM signal forwarding to pid [$child]."
+  kill -TERM "$child" 2> /dev/null
+  waitForChildProcessToFinish
+}
+
+#sigint caught SIGINT signal and forward it to child process
+_sigint() {
+  echo "[entrypoint.sh] caught SIGINT signal forwarding to pid [$child]."
+  kill -INT "$child" 2> /dev/null
+  waitForChildProcessToFinish
+}
+
+#waitForChildProcessToFinish waits for child process to finish
+waitForChildProcessToFinish(){
+    while ps -p "$child" > /dev/null; do sleep 1; done;
+}
+
+trap _sigint INT
+trap _sigterm SIGTERM
+
+wait $child
